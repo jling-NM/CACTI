325cfca557d202fb1d9515b6b90c3299b8d21356
josef ling <jling@swcp.com>
2016-07-21 23:15:26 -0600

    Fixed utterance coding resolution.

--- src/edu/unm/casaa/main/Utils.java
+++ src/edu/unm/casaa/main/Utils.java
@@ -5,14 +5,14 @@ import javafx.util.Duration;
 /**
  *
  */
-public class Utils {
+class Utils {
 
     /**
      * Newer format using Duration
-     * @param duration
-     * @return
+     * @param duration time
+     * @return time converted to string
      */
-    public static String formatDuration(Duration duration) {
+    static String formatDuration(Duration duration) {
         double duration_secs = duration.toSeconds();
         return String.format("%02.0f:%02.0f:%02.0f", Math.floor((duration_secs/3600) % 24), Math.floor( (duration_secs/60) % 60), (duration_secs % 60) );
     }
@@ -22,16 +22,17 @@ public class Utils {
      * @param time time to convert
      * @return byte position
      */
-    public static int convertTimeToBytes( int bytesPerSecond, Duration time ) {
-        return ((int) time.toSeconds()) * bytesPerSecond;
+    static int convertTimeToBytes(int bytesPerSecond, Duration time) {
+        // truncation cast to int to cut off '.0' introduced by double seconds
+        return (int) ( time.toSeconds() * bytesPerSecond );
     }
 
     /**
-     * @param filename
+     * @param filename input filename to change
      * @param newSuffix Suffixes should be specified without leading period.
      * @return copy of filename with oldSuffix (if present) removed, and newSuffix added.
      */
-    public static String changeSuffix( String filename, String newSuffix ) {
+    static String changeSuffix(String filename, String newSuffix) {
         String	result 	= filename;
         int		index 	= filename.lastIndexOf( '.' );
 
